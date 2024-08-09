5ef95dc6054f7511f14d738158c5d401efbaef26
johnsonhabarugira <habarugirajohnson@gmail.com>
2024-08-09 07:50:51 +0300

    building menus

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request,"website/index.html")
